6321fcac27023334395dc8b8058279f07feedb2a
roman-orekhov <roman.s.orekhov@gmail.com>
2024-04-13 15:42:28 +0300

    fix setting caption while initializing controls (Name should be set first since it autosets caption)

--- src/LuaController.pas
+++ src/LuaController.pas
@@ -108,15 +108,15 @@ procedure InitControl(L: Plua_State; luaObj:TObject; var Name:String);
 var
   tindex: Integer;
 begin
-     tindex := lua_gettop(L) - 1;
-     if (tindex>0) and (lua_istable(L,tindex)) and (GetLuaObjectUnsafePop(L,tindex) = nil) then
-        UpdatePropertiesFromLuaTable(L, luaObj.ClassName, tindex + 1, tindex, luaObj);
-     try
-        (luaObj as TComponent).Name := Name;
-     except
-         on E: Exception do
-            LuaError(L, E.ClassName , E.Message );
-     end;
+  try
+     (luaObj as TComponent).Name := Name;
+  except
+      on E: Exception do
+         LuaError(L, E.ClassName, E.Message);
+  end;
+  tindex := lua_gettop(L) - 1;
+  if (tindex>0) and (lua_istable(L,tindex)) and (GetLuaObjectUnsafePop(L,tindex) = nil) then
+     UpdatePropertiesFromLuaTable(L, luaObj.ClassName, tindex + 1, tindex, luaObj);
 end;
 
 function ControlFree(L: Plua_State): Integer; cdecl;
